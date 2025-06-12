82a4b7ef0311c1236d9d22191340a15327fc1270
Peter Lourens <peter@lourens.cloud>
2025-06-12 20:17:26 +0200

    update bt code

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,71 @@ BluetoothSerial SerialBT;
 
 void setup() {
 
-	int data;
+	pinMode(32,OUTPUT);
+	pinMode(33,OUTPUT);
+	pinMode(25,OUTPUT);
+	pinMode(26,OUTPUT);
+
 	Serial.begin(115200);
+
+	digitalWrite(32,LOW);
+	digitalWrite(33,LOW);
+	digitalWrite(25,LOW);
+	digitalWrite(26,LOW);
 
 	SerialBT.begin("ESP32_Slave"); // Bluetooth device name
 	SerialBT.setPin("1234"); // Set the PIN for pairing
-	Serial.println("The device started, now you can pair it with Bluetooth!");
-
 }
 
 void loop() { 
 
-	/*if (Serial.available()) {
-		int data = Serial.read();
-
-	if (data >= 0 && data <= 255) {
-		SerialBT.write(data);
-	}
-	}*/
+	String RxBuffer = "";
+	char RxByte;
 
 	if (SerialBT.available()) {
-		int data = SerialBT.read();
-		Serial.println(data);
+		
+		while (SerialBT.available() > 0) {
+      		RxByte = SerialBT.read();
+			RxBuffer += String(RxByte);
+		}
+
+		Serial.print("\n");
+		Serial.print(RxBuffer);
+
+		if (RxBuffer == "low") {
+			digitalWrite(32,HIGH);
+			digitalWrite(33,LOW);
+			digitalWrite(25,LOW);
+			digitalWrite(26,LOW);
+		}
+		else if (RxBuffer == "medium") {
+			digitalWrite(32,LOW);
+			digitalWrite(33,HIGH);
+			digitalWrite(25,LOW);
+			digitalWrite(26,LOW);
+		}
+		else if (RxBuffer == "high") {
+			digitalWrite(32,LOW);
+			digitalWrite(33,LOW);
+			digitalWrite(25,HIGH);
+			digitalWrite(26,LOW);
+		}
+		else {
+			digitalWrite(32,LOW);
+			digitalWrite(33,LOW);
+			digitalWrite(25,LOW);
+			digitalWrite(26,HIGH);
+		}
+
+
+		vTaskDelay(5000);
+		
+		digitalWrite(32,LOW);
+		digitalWrite(33,LOW);
+		digitalWrite(25,LOW);
+		digitalWrite(26,LOW);
+
+
 	}
 }
 
