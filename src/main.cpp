315c012f201a37bf1c2036043ed8f015f611f9fd
peter <peter@lourens.cloud>
2025-06-19 19:26:51 +0200

    Update char to int function and correction LED logic

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,13 @@ void onReceive(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
 
     Serial.printf("\n\nReceived fanspeed: %s\n", fanspeed);
 
-	//Char array is received, convert to int
-	long int fanspeed_int = strtol(fanspeed, NULL, 10);
+    int fanspeed_int;
 	
-	Serial.printf("\nReceived fanspeed number: %s\n", fanspeed_int);
+    //Char array is received, convert to int
+	fanspeed_int = strtol(fanspeed, NULL, 10);
+    
+    Serial.print("\nfanspeed int: ");
+    Serial.print(fanspeed_int);
 
 	if (fanspeed_int > 0 && fanspeed_int <= 20 ) {
         //Low speed
@@ -27,7 +30,7 @@ void onReceive(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
         digitalWrite(25,LOW);
         digitalWrite(26,LOW);
     }
-	if (fanspeed_int > 50 && fanspeed_int < 100) {
+	else if (fanspeed_int > 50 && fanspeed_int < 100) {
         //High speed
 		digitalWrite(32,LOW);
 		digitalWrite(33,LOW);
@@ -58,10 +61,9 @@ void setup() {
 
     WiFi.mode(WIFI_STA);
     esp_now_init();
-
     esp_now_register_recv_cb(onReceive);
 }
 
 void loop() {
-
+    vTaskDelay(20000);
 }
